9944bc8de57f94dfc302de4ed0f266e5db7b50e9
Mar-Nb <m.niombela@cfa-insta.fr>
2022-02-26 16:54:41 +0100

    Ajout de CSS de transition pour le front-end

--- front-end/script.js
+++ front-end/script.js
@@ -1,27 +1,49 @@
 document.getElementById("send-msg").addEventListener("click", sendMessage);
+document.getElementById("zone-msg").addEventListener("keyup", function(evt) {
+	if (evt.key == "Enter") { sendMessage(); }
+});
 
 function sendMessage() {
 	msg = document.getElementById("zone-msg").value;
 	document.getElementById("zone-msg").value = "";
-	document.getElementById("lst-conv").appendChild(createBulle(msg, true));
+	createBulle(msg, true);
 	responseBot(msg);
+	document.getElementsByTagName("main")[0].scrollTop = document.getElementsByTagName("main")[0].scrollHeight;
 }
 
 function createBulle(txt, isUser) {
 	bulle = document.createElement("li");
 	bulle.classList.add(isUser ? "user-msg" : "bot-msg");
-
+	
 	bulleDiv = document.createElement("div");
 	spanTexte = document.createElement("span");
 	txtBulle = document.createTextNode(txt);
 	spanTexte.appendChild(txtBulle);
 	bulleDiv.appendChild(spanTexte); 
 	bulle.appendChild(bulleDiv);
+	
+	document.getElementById("lst-conv").appendChild(bulle);
+}
 
-	return bulle;
+function loadingBulle() {
+	bulle = document.createElement("li");
+	bulle.classList.add("bot-msg");
+	bulleDiv = document.createElement("div");
+	spanImage = document.createElement("span");
+	imageLoad = document.createElement("img");
+
+	imageLoad.src = "loader-bibi.gif";
+	imageLoad.width = 30;
+	bulle.id = "loader";
+	
+	spanImage.appendChild(imageLoad);
+	bulleDiv.appendChild(spanImage);
+	bulle.appendChild(bulleDiv);
+	document.getElementById("lst-conv").appendChild(bulle);
 }
 
 function responseBot(question) {
+	loadingBulle();
 	var ajax = new XMLHttpRequest();
 	var formData = new FormData();
 
@@ -29,7 +51,10 @@ function responseBot(question) {
 	ajax.open("POST", "http://localhost:5000/bibi-reponse");
 	ajax.onload = function() {
 		res = JSON.parse(ajax.response);
-		document.getElementById("lst-conv").appendChild(createBulle(res.reponse, false));
+		console.log(res);
+		document.getElementById("loader").remove();
+		createBulle(res.reponse, false);
+		document.getElementsByTagName("main")[0].scrollTop = document.getElementsByTagName("main")[0].scrollHeight;
 	}
 
 	ajax.send(formData);
